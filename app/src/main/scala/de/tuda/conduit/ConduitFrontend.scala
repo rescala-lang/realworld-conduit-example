87b92ec0b7f5027ed71883beeef522af1922ea14
ragnar <ragnar@rmgk.de>
2020-03-02 17:11:19 +0100

    handle registration and login

--- app/src/main/scala/de/tuda/conduit/ConduitFrontend.scala
+++ app/src/main/scala/de/tuda/conduit/ConduitFrontend.scala
@@ -25,7 +25,7 @@ import upickle.default.ReadWriter
 import scala.concurrent.{ExecutionContext, Future}
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js.{UndefOr, |}
-import scala.util.{Failure, Random}
+import scala.util.{Failure, Random, Try}
 
 
 //@JSImport("localforage", JSImport.Namespace)
@@ -71,10 +71,12 @@ object API {
   case class Comment(id: Int, createdAt: String, updatedAt: String, body: String, author: Author)
   case class CommentList(comments: List[Comment])
 
-  case class UserReg(username: String, password: String, email: String, bio: Option[String] = None, image: Option[String] = None)
-  case class User(id: Int, token: String, username: String, password: String, email: String, bio: Option[String] = None, image: Option[String] = None)
+  case class UserReg(username: String = "", password: String, email: String, bio: String = "", image: String = "")
+  case class User(id: Int, token: String, username: String, email: String, bio: Option[String] = None, image: Option[String] = None)
   case class UserRegWrapper(user: UserReg)
   case class UserWrapper(user: User)
+
+  case class ErrorMessages(errors: Map[String, List[String]])
 
 
   implicit val AuthorRW        : ReadWriter[Author]         = upickle.default.macroRW
@@ -86,6 +88,7 @@ object API {
   implicit val UserRegRW       : ReadWriter[UserReg]        = upickle.default.macroRW
   implicit val UserRegWrapperRW: ReadWriter[UserRegWrapper] = upickle.default.macroRW
   implicit val UserWrapperRW   : ReadWriter[UserWrapper]    = upickle.default.macroRW
+  implicit val ErrorMessagesRW : ReadWriter[ErrorMessages]  = upickle.default.macroRW
 
   def articles(): Future[List[Article]] =
     Fetch.fetch(baseurl + "/articles").toFuture.flatMap(_.text().toFuture)
@@ -100,39 +103,62 @@ object API {
          }
   }
 
-  def registerFun(user: UserReg): Future[User] = {
+  def loginRegisterFun(user: UserReg, endpoint: String): Future[Either[ErrorMessages, User]] = {
     val serializedUser = upickle.default.write(UserRegWrapper(user))
     println(serializedUser)
-    Fetch.fetch(baseurl + "/users",
+    Fetch.fetch(baseurl + endpoint,
                 js.Dynamic.literal(
                   method = HttpMethod.POST,
                   body = serializedUser,
                   headers = js.Dictionary("Content-Type" -> "application/json;charset=utf-8")
                   ).asInstanceOf[RequestInit]).toFuture.flatMap(_.text().toFuture)
          .map { response =>
-           upickle.default.read[UserWrapper](response).user
+           try {Right(upickle.default.read[UserWrapper](response).user)}
+           catch {
+             case error: Throwable =>
+               println(error)
+               Left(upickle.default.read[ErrorMessages](response))
+           }
          }
 
   }
 
-  def loginFun(user: UserReg): Future[User] = {
-    val serializedUser = upickle.default.write(UserRegWrapper(user))
-    Fetch.fetch(baseurl + "/users/login",
-                js.Dynamic.literal(
-                  method = HttpMethod.POST,
-                  body = serializedUser,
-                  headers = js.Dictionary("Content-Type" -> "application/json;charset=utf-8")
-                  ).asInstanceOf[RequestInit]).toFuture.flatMap(_.text().toFuture)
-         .map { response =>
-           upickle.default.read[UserWrapper](response).user
-         }
+  def registerFun(user: UserReg): Future[Either[ErrorMessages, User]] = {
+    loginRegisterFun(user, endpoint = "/users")
 
   }
 
-  val login: PullEvent[UserReg, User, default.Structure] = PullEvent.from(loginFun)
-  val register: PullEvent[UserReg, User, default.Structure] = PullEvent.from(registerFun)
-  val currentUser: Signal[Option[User]] = (login.event || register.event).latestOption()
+  def loginFun(user: UserReg): Future[Either[ErrorMessages, User]] = {
+    loginRegisterFun(user, endpoint = "/users/login")
+  }
 
+  val login   : PullEvent[UserReg, Either[ErrorMessages, User], default.Structure] = PullEvent.from(loginFun)
+  val register: PullEvent[UserReg, Either[ErrorMessages, User], default.Structure] = PullEvent.from(registerFun)
+
+
+  val loginresult                       = login.event || register.event
+  val currentUser: Signal[Option[User]] =
+    StorageManager.stored[Option[User]]("currentUser") { restored =>
+      loginresult.map[Option[User]](_.toOption).latest(restored.flatten)
+    }
+
+  val errorMessages: Signal[Option[ErrorMessages]] = loginresult.map(_.swap.toOption).latest(None)
+
+  Templates.authentication.loginUser.observe(login(_))
+  Templates.authentication.registerUser.observe(register(_))
+
+}
+
+object StorageManager {
+  val storage = dom.window.localStorage
+  def get[T: upickle.default.Reader](key: String): Option[T] =
+    Option(storage.getItem(key)).flatMap(str => Try {upickle.default.read(str)}.toOption)
+
+  def stored[T: ReadWriter](key: String)(create: Option[T] => Signal[T]): Signal[T] = {
+    val res = create(get[T](key))
+    res.observe(value => storage.setItem(key, upickle.default.write(value)))
+    res
+  }
 }
 
 trait PullEvent[A, T, S <: Struct] {
@@ -179,12 +205,13 @@ object ConduitFrontend {
     val currentArticle = Templates.articleFromSlug(slug, mainArticles)
 
 
-    dom.document.body = body(Templates.navTag(Navigation.currentAppState),
+    dom.document.body = body(Templates.navTag(Navigation.currentAppState, API.currentUser),
                              Signal {
                                Navigation.currentAppState.value match {
                                  case Index            => Templates.articleList(mainArticles)
                                  case Settings         => Templates.settings
-                                 case Login | Register => Templates.signup
+                                 case Register         => Templates.authentication.register(API.errorMessages)
+                                 case Login            => Templates.authentication.login(API.errorMessages)
                                  case Compose          => Templates.createEdit
                                  case Author(username) => Templates.profile
                                  case Reader(slug)     => currentArticle.value
@@ -205,6 +232,22 @@ object Navigation {
   sealed abstract class AppState(val urlhash: String) {
     def url = s"#$urlhash"
   }
+  object AppState {
+    def parse(path: String): AppState = {
+      val paths = List(path.substring(1).split("/"): _*)
+      scribe.debug(s"get state for $paths")
+      paths match {
+        case Nil | "" :: Nil              => Index
+        case "settings" :: Nil            => Settings
+        case "login" :: Nil               => Login
+        case "register" :: Nil            => Register
+        case "compose" :: Nil             => Compose
+        case "reader" :: slug :: Nil      => Reader(slug)
+        case "profile" :: username :: Nil => Author(username)
+        case _                            => Index
+      }
+    }
+  }
   case object Index extends AppState("")
   case object Settings extends AppState("settings")
   case object Login extends AppState("login")
@@ -214,21 +257,6 @@ object Navigation {
   case class Reader(slug: String) extends AppState(s"reader/$slug")
 
 
-  def pathToState(path: String): AppState = {
-    val paths = List(path.substring(1).split("/"): _*)
-    scribe.debug(s"get state for $paths")
-    paths match {
-      case Nil | "" :: Nil              => Index
-      case "settings" :: Nil            => Settings
-      case "login" :: Nil               => Login
-      case "register" :: Nil            => Register
-      case "compose" :: Nil             => Compose
-      case "reader" :: slug :: Nil      => Reader(slug)
-      case "profile" :: username :: Nil => Author(username)
-      case _                            => Index
-    }
-  }
-
   def getHash: String = {
     dom.window.location.hash
   }
@@ -237,12 +265,12 @@ object Navigation {
     Events.fromCallback[HashChangeEvent](dom.window.onhashchange = _).event
   hashChange.observe(hc => scribe.debug(s"hash change event: ${hc.oldURL} -> ${hc.newURL}"))
 
-  val hashBasedStates = hashChange.map(hc => pathToState(new URL(hc.newURL).hash))
+  val hashBasedStates = hashChange.map(hc => AppState.parse(new URL(hc.newURL).hash))
 
 
   val targetStates: Event[AppState] = hashBasedStates
 
-  val initialState = pathToState(getHash)
+  val initialState = AppState.parse(getHash)
 
   scribe.debug(s"initial state: $initialState")
 
--- app/src/main/scala/de/tuda/conduit/Templates.scala
+++ app/src/main/scala/de/tuda/conduit/Templates.scala
@@ -1,10 +1,10 @@
 package de.tuda.conduit
 
-import de.tuda.conduit.API.{Article, Author, Comment}
+import de.tuda.conduit.API.{Article, Author, Comment, ErrorMessages, User, UserReg}
 import de.tuda.conduit.Navigation.AppState
 import org.scalajs.dom.Element
 import scalatags.JsDom.all._
-import scalatags.JsDom.tags2._
+import scalatags.JsDom.tags2.{SeqFrag => _, _}
 import rescala.default._
 import rescala.extra.Tags._
 import rescala.reactives.RExceptions.EmptySignalControlThrowable
@@ -12,25 +12,39 @@ import scalatags.JsDom.TypedTag
 
 object Templates {
 
-  def navTag(currentPlace: Signal[AppState]) =
+  def navTag(currentPlace: Signal[AppState], currentUser: Signal[Option[User]]) = {
+
+    val navlist =
+      currentUser.map[List[(AppState, Modifier)]] {
+        case None       => List(
+          Navigation.Index -> stringFrag("""Home"""),
+          Navigation.Login -> stringFrag("""Log in"""),
+          Navigation.Register -> stringFrag("""Sign up""")
+          )
+        case Some(user) => List(
+          Navigation.Index -> stringFrag("""Home"""),
+          Navigation.Compose -> i(`class` := "ion-compose", raw("&nbsp;New article")),
+          Navigation.Settings -> i(`class` := "ion-gear-a", raw("&nbsp;Settings")),
+          Navigation.Author(user.username) -> stringFrag(user.username)
+          )
+      }
+
+    val listitems = navlist.map { items =>
+      items.map { case (place, description) =>
+        li(
+          `class` := currentPlace.map { as =>
+            (if (as == place) "nav-item active" else "nav-item")
+          },
+          a(`class` := "nav-link", href := place.url, description))
+      }
+    }
+
     nav(`class` := "navbar navbar-light",
         div(`class` := "container",
             a(`class` := "navbar-brand", href := Navigation.Index.url, "conduit"),
-            ul(`class` := "nav navbar-nav pull-xs-right")(
-              List(
-                Navigation.Index -> raw("""Home"""),
-                Navigation.Compose -> raw("""<i class="ion-compose"></i>&nbsp;New Post"""),
-                Navigation.Settings -> raw("""<i class="ion-gear-a"></i>&nbsp;Settings"""),
-                Navigation.Login -> raw("""Log in"""),
-                Navigation.Register -> raw("""Sign up""")
-                ).map { case (place, description) =>
-                li(
-                  `class` := currentPlace.map { as =>
-                    (if (as == place) "nav-item active" else "nav-item")
-                  },
-                  a(`class` := "nav-link", href := place.url, description))
-              }: _*
-              )))
+            ul(`class` := "nav navbar-nav pull-xs-right")(listitems.asModifierL))
+        )
+  }
 
   val footerTag = footer(raw("""
       <div class="container">
@@ -126,26 +140,67 @@ object Templates {
         )
   }
 
-  val signup = div(`class` := "auth-page",
-                   div(`class` := "container page",
-                       div(`class` := "row",
-                           div(`class` := "col-md-6 offset-md-3 col-xs-12",
-                               h1(`class` := "text-xs-center", "Sign up"),
-                               p(`class` := "text-xs-center", a(href := "", "Have an account?")),
-                               ul(`class` := "error-messages", li("That email is already taken")),
-                               fieldset(`class` := "form-group",
-                                        input(`class` := "form-control form-control-lg",
-                                              `type` := "text",
-                                              placeholder := "Your Name")),
-                               fieldset(`class` := "form-group",
-                                        input(`class` := "form-control form-control-lg",
-                                              `type` := "text",
-                                              placeholder := "Email")),
-                               fieldset(`class` := "form-group",
-                                        input(`class` := "form-control form-control-lg",
-                                              `type` := "password",
-                                              placeholder := "Password")),
-                               button(`class` := "btn btn-lg btn-primary pull-xs-right", "Sign up")))))
+  object authentication {
+
+
+    val nameInput = input(`class` := "form-control form-control-lg",
+                          `type` := "text",
+                          placeholder := "Your Name").render
+
+    val mailInput     = input(`class` := "form-control form-control-lg",
+                              `type` := "text",
+                              placeholder := "Email").render
+    val passwordInput = input(`class` := "form-control form-control-lg",
+                              `type` := "password",
+                              placeholder := "Password").render
+
+    private val registerEvt = Evt[Any]
+    val registerUser = registerEvt.map { _ =>
+      UserReg(username = nameInput.value, password = passwordInput.value, email = mailInput.value)
+    }
+
+    private val loginEvt = Evt[Any]
+    val loginUser = loginEvt.map { _ =>
+      UserReg(password = passwordInput.value, email = mailInput.value)
+    }
+
+
+    def wrapping(inner: Tag*) = {
+      div(`class` := "auth-page",
+          div(`class` := "container page",
+              div(`class` := "row",
+                  div(`class` := "col-md-6 offset-md-3 col-xs-12", SeqFrag(inner)))))
+    }
+
+
+    def errors(messages: Signal[Option[ErrorMessages]]) = {
+      val listitems = messages.map(m => m.map(_.errors).getOrElse(Map.empty).toList.map {
+        case (name, message) => li(name, " ", message.mkString(" and "))
+      })
+      ul(`class` := "error-messages", listitems.asModifierL)
+    }
+
+
+    def register(messages: Signal[Option[ErrorMessages]]) = wrapping(
+      h1(`class` := "text-xs-center", "Sign up"),
+      p(`class` := "text-xs-center", a(href := Navigation.Login.url, "Have an account?")),
+      errors(messages),
+      form(
+        onsubmit := registerEvt,
+        SeqFrag(List(nameInput, mailInput, passwordInput).map(ip => fieldset(`class` := "form-group", ip))),
+        button(`class` := "btn btn-lg btn-primary pull-xs-right", "Sign up")
+        ))
+
+    def login(messages: Signal[Option[ErrorMessages]]) = wrapping(
+      h1(`class` := "text-xs-center", "Log in"),
+      p(`class` := "text-xs-center", a(href := Navigation.Register.url, "Create account?")),
+      errors(messages),
+      form(
+        onsubmit := loginEvt,
+        SeqFrag(List(mailInput, passwordInput).map(ip => fieldset(`class` := "form-group", ip))),
+        button(`class` := "btn btn-lg btn-primary pull-xs-right", "Log in")
+        ))
+  }
 
   val profile = div(`class` := "profile-page", raw("""
         <div class="user-info">
@@ -304,7 +359,9 @@ object Templates {
                       """))
 
   def articleFromSlug(slug: Signal[String], articles: Signal[List[Article]]): Signal[TypedTag[Element]] = {
-    Signal[TypedTag[Element]] {articles.value.find(_.slug == slug.value).map(articleTag).getOrElse(throw EmptySignalControlThrowable)}
+    Signal[TypedTag[Element]] {
+      articles.value.find(_.slug == slug.value).map(articleTag).getOrElse(throw EmptySignalControlThrowable)
+    }
   }
 
   def articleTag(article: Article): TypedTag[Element] = {
